567414d03e0fc73478357270ba9cb56f27aa68dc
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-06 23:05:18 +0500

    080808

--- For08.py
+++ For08.py
@@ -1,3 +1,4 @@
+
 def main(N):
     """
     The number N is given. Calculate the sum below: 1 + 1/2 + 1/3 + … + 1/N.
@@ -7,11 +8,16 @@ def main(N):
         float: return  answer
     """
     a=0
-    
+    b=0
     list=[]
-    while  a<N:
+    while a<N:
         list=list+[a+1]
+        if list[a]>0:
+            x=list[a]
+            b+=1/x
         a+=1
-    return list
-print(main(9))
+    return b 
+print(main(4))
+
+
 
