6b67bdcf9c77de6d749c3656a77a7e3e77f9dde1
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-06 20:55:40 +0500

    0606

--- For06.py
+++ For06.py
@@ -7,4 +7,32 @@ def main(A,B):
     Returns:
         int: return  answer
     """
-    return
+    a=0
+    b=0
+
+    list1=[]
+   
+    while a<A:
+        list1=list1+[a]
+        if list1[a]%1==0:
+            x=list1[a]
+            b+=x
+            a+=1
+    
+    list2=[]
+    f=0
+    p=0
+    while f<B:
+        list2=list2+[f]
+        if list2[f]%1==0:
+            y=list2[f]
+            p+=y
+            f+=1
+    return p-b
+print(main(-6,8))
+
+
+
+
+    
+  
